165f5627a5fe8de9b17314fb0b5a1eb553ea5346
Ingyin275 <168635856+Ingyin275@users.noreply.github.com>
2024-05-03 08:41:07 +0630

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+var config = {
+   
+    lines: [
+        "Dear💚",
+        "I've never seen you outside, ",
+        "but I can't stop thinking about you.",
+        "Your presence somehow captivates me.",
+        "I know it's unexpected, ",
+        "but I've developed feelings for you.",
+        "I had to be honest. ",
+        "Your response matters, but I value our connection.",
+        "I Love You.",
+    ],
+   
+    date: "2018-05-11T23:59:59",
+ 
+    names: ["JK", "JK"]
+}
+
